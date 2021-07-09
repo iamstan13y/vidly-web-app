aec966a3654c9764f0ef032cda721850c31c2040
iamstan13y <iamstan13y@gmail.com>
2021-07-09 10:34:14 +0200

    added migrations.

---
+++ Vidly/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Vidly.Models.MovieContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Vidly.Models.MovieContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ Vidly/Models/MovieContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext()
+        {
+
+        }
+    }
+}
